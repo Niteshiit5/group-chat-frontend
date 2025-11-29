8913eeb250902846245bc6d5ff9692447cb9eeea
Neetesh Kumar <neeteshiit5@gmail.com>
2025-11-29 20:43:12 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,40 @@
+const socket = io('http://localhost:4000');
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+let name;
+do {
+    name = prompt("Enter your name to join");
+} while (!name || name.trim() === "");
+
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name => {
+    append(`${name} :JOINED THE CHAT`, 'right');
+});
+// message recieve
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+
+})
+
+
